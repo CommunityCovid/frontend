c729b449af53606abd4a343fe034b185a39fb577
ArslanaWu <50170078+ArslanaWu@users.noreply.github.com>
2022-08-29 19:17:41 +0800

    Draw grids

--- src/store/models/datastore.js
+++ src/store/models/datastore.js
@@ -1,24 +1,24 @@
-import {GridModel} from "../../models/gridModel";
-import api from "@/service/grids";
+// import {GridModel} from "@/models/gridModel";
+// import api from "@/service/grids";
 
 const state = () => ({
-    grids: [],
-    gridMap: new Map()
+    // grids: [],
+    // gridMap: new Map()
 })
 
-const actions = {}
+const actions = {};
 
 const mutations = {
-    async initGrids(state) {
-        const res = await api.getGridsCnt();
-        const gridsData = res["data"][0];
-        for (const key in gridsData) {
-            let grid = new GridModel(key, gridsData[key]["totalCnt"], gridsData[key]["finishedCnt"])
-            state.grids.push(grid)
-            state.gridMap.set(key, grid)
-        }
-    }
-}
+    // async initGrids(state) {
+    //     const res = await api.getGridsCnt();
+    //     const gridsData = res["data"][0];
+    //     for (const key in gridsData) {
+    //         let grid = new GridModel(key, gridsData[key]["totalCnt"], gridsData[key]["finishedCnt"])
+    //         state.grids.push(grid)
+    //         state.gridMap.set(key, grid)
+    //     }
+    // }
+};
 
 export default {
     namespaced: true,
--- src/views/MainView/GridView.vue
+++ src/views/MainView/GridView.vue
@@ -1,25 +1,104 @@
 <template>
-  <div id="grid-view">
-    <div v-if="grids !== undefined">
-      <DoughnutChart v-for="grid in grids" :key="grid.name" :grid="grid"></DoughnutChart>
-    </div>
-  </div>
+  <div id="grid-view" v-if="grids !== undefined" ref="gridview"/>
 </template>
 
 <script>
 import {mapState} from "vuex";
-import DoughnutChart from "../../components/DoughnutChart";
+import api from "@/service/grids";
 
 export default {
   name: "GridView",
-  components: {DoughnutChart},
   created() {
-    this.$store.commit("datastore/initGrids")
+    this.$store.commit("datastore/initGrids");
+  },
+  mounted() {
+    this.drawGrids();
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    async drawGrids() {
+      const gridsCnt = await api.getGridsCnt();
+      const gridsData = gridsCnt["data"][0];
+
+      let myChart = this.$echarts.getInstanceByDom(this.$refs["gridview"]);
+      if (myChart == null) {
+        myChart = this.$echarts.init(this.$refs["gridview"]);
+      }
+
+      let option = {
+        tooltip: {
+          trigger: "item"
+        },
+        legend: {
+          top: "3.3%",
+          left: "center",
+          height: "3.3%"
+        },
+        series: []
+      };
+      let config = {
+        type: "pie",
+        radius: ["4%", "8%"],
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: "#fff",
+          borderWidth: 2
+        },
+        label: {
+          show: true,
+          position: "center",
+          formatter: `{a}`
+        },
+        labelLine: {
+          show: false
+        }
+      };
+      let cnt = 0;
+      for (let grid in gridsData) {
+        const data = gridsData[grid];
+        const x = Math.floor(cnt / 10);
+        const y = cnt - x * 10;
+
+        let newConfig = {...config};
+        newConfig["name"] = grid;
+        newConfig["center"] = [`${y * 10 + 5}%`, `${x * 10 + 15}%`];
+        newConfig["data"] = [
+          {value: data["finishedCnt"], name: "finished"},
+          {value: data["totalCnt"] - data["finishedCnt"], name: "unfinished"}
+        ];
+        option["series"].push(newConfig);
+
+        cnt += 1;
+      }
+
+      myChart.on("click", function (params) {
+        console.log(params);
+
+        if (params.componentType === "series") {
+          if (params.seriesType === "graph") {
+            if (params.dataType === "edge") {
+              // 点击到了 graph 的 edge（边）上。
+            } else {
+              // 点击到了 graph 的 node（节点）上。
+            }
+          }
+        }
+      });
+
+      myChart.setOption(option);
+    }
   },
   computed: {
-    ...mapState('datastore', {
+    ...mapState("datastore", {
       grids: state => state.grids
     })
+  },
+  watch: {
+    grids() {
+
+    }
   }
 };
 </script>
@@ -27,5 +106,6 @@ export default {
 <style scoped lang="scss">
 #grid-view {
   height: 100%;
+  width: 100%;
 }
 </style>
