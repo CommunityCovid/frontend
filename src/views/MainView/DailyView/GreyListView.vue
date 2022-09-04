0285a374a5ab1c4c08824f51d8149953f9572439
ArslanaWu <50170078+ArslanaWu@users.noreply.github.com>
2022-09-04 21:07:43 +0800

    Pagination

--- src/views/MainView/DailyView/GreyListView.vue
+++ src/views/MainView/DailyView/GreyListView.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="grey-list-view">
     <span id="title">灰名单列表</span>
+
     <div id="grey-list-table" ref="greylistview">
-      <el-table v-if="tableHeight"
-                :data="greyListPeople"
-                stripe :max-height="tableHeight">
+      <el-table v-if="tableHeight && greyListPeople"
+                :data="getPageData()"
+                stripe :height="tableHeight" :max-height="tableHeight">
         <el-table-column v-for="(item, index) in columns" :key="`${index}${item}`"
                          :label="item" :width="wideColumns.includes(item)?'210px':'120px'">
           <template slot-scope="scope">
@@ -12,6 +13,15 @@
           </template>
         </el-table-column>
       </el-table>
+
+      <div id="pagination">
+        <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="total"
+            @current-change="currentChange">
+        </el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -27,12 +37,14 @@ export default {
       columns: null,
       greyListPeople: null,
       wideColumns: ["证件号码", "出生年月", "户籍地址", "工作单位地址",
-        "楼栋编码", "房屋编码", "审核时间", "灰名单原因", "上次核酸检测时间"]
+        "楼栋编码", "房屋编码", "审核时间", "灰名单原因", "上次核酸检测时间"],
+      pageSize: 10,
+      currentPage: 1,
+      total: 0
     };
   },
   mounted() {
     this.tableHeight = this.$refs.greylistview.clientHeight;
-
     this.getGreyList();
   },
   methods: {
@@ -46,7 +58,14 @@ export default {
           return result;
         }, {});
       });
-      this.greyListPeople = this.greyListPeople.slice(2, 20); // todo
+      this.total = this.greyListPeople.length;
+    },
+    currentChange(currentPage) {
+      this.currentPage = currentPage;
+    },
+    getPageData() {
+      return this.greyListPeople.slice((this.currentPage - 1) * this.pageSize,
+          this.currentPage * this.pageSize);
     }
   }
 };
@@ -64,7 +83,12 @@ export default {
 
   #grey-list-table {
     width: 100%;
-    height: calc(100% - 45px);
+    height: calc(100% - 100px);
+  }
+
+  #pagination {
+    text-align: center;
+    margin-top: 10px;
   }
 }
 </style>
